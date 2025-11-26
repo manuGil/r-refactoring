5333c42d1fdca9bf20687b6cf4d77d9d6695e17b
Manuel Garcia <gilbert.mg22@gmail.com>
2025-11-26 15:17:53 +0100

    Fix comment syntax in 1Nov_AmsterdamAmenities.R

--- amterdam-amenities/1Nov_AmsterdamAmenities.R
+++ amterdam-amenities/1Nov_AmsterdamAmenities.R
@@ -9,7 +9,7 @@
 
 ## 1 November - Points
 
-"30DayMapChallenge classic: A map with points. Start the challenge with points. Show individual locations—anything from cities to trees or more abstract concepts. Simple, but a key part of the challenge."
+# "30DayMapChallenge classic: A map with points. Start the challenge with points. Show individual locations—anything from cities to trees or more abstract concepts. Simple, but a key part of the challenge."
 
 ### 1. Load packages
 
